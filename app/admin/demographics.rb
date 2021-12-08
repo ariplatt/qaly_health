b276e365ea34219285228a756a27f4c428329744
anonymous anonymous <ari.platt@kellogg.northwestern.edu>
2021-12-08 19:47:34 +0000

    Whitelist columns in Demographic for ActiveAdmin

--- app/admin/demographics.rb
+++ app/admin/demographics.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Demographic do
 
+ permit_params :email, :password, :user_name, :disease_name, :disease_id, :date_diagnosed, :time_diagnosed, :description_of_illness
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
